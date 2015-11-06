807e08cfeb7d945ff16c7956c3db5ad0d2394898
Shaun McGlinchey <shaunmcglinchey@users.noreply.github.com>
2015-11-06 01:28:04 -0300

    added AuthTokenFactory and AuthTokenInterceptor

--- public/app.js
+++ public/app.js
@@ -1,7 +1,9 @@
 (function() {
    'use strict';
 
-    var app = angular.module('app', []);
+    var app = angular.module('app', [], function config($httpProvider) {
+        $httpProvider.interceptors.push('AuthInterceptor');
+    });
 
     app.constant('API_URL', 'http://localhost:5000/api');
 
@@ -19,7 +21,7 @@
 
         function login(username, password) {
             UserFactory.login(username, password).then(function success(response) {
-               vm.user = response.data;
+               vm.user = response.data.user;
             }, handleError);
         }
 
@@ -38,7 +40,7 @@
         }
     });
 
-    app.factory('UserFactory', function UserFactory($http, API_URL) {
+    app.factory('UserFactory', function UserFactory($http, API_URL, AuthTokenFactory) {
        'use strict';
         return {
             login: login
@@ -47,7 +49,51 @@
             return $http.post(API_URL + '/login', {
                 username: username,
                 password: password
+            }).then(function success(response) {
+                AuthTokenFactory.setToken(response.data.token);
+                return response;
             })
         }
     });
+
+    app.factory('AuthTokenFactory', function AuthTokenFactory($window) {
+        'use strict';
+        var store = $window.localStorage;
+        var key = 'auth-token';
+
+        return {
+            getToken: getToken,
+            setToken: setToken
+        };
+
+        function getToken() {
+            return store.getItem(key);
+        }
+
+        function setToken(token) {
+            if (token) {
+                store.setItem(key, token);
+            } else {
+                store.removeItem(key);
+            }
+        }
+
+    });
+
+    app.factory('AuthInterceptor', function AuthInterceptor(AuthTokenFactory) {
+        'use strict';
+        return {
+            request: addToken
+        };
+
+        function addToken(config) {
+            var token = AuthTokenFactory.getToken();
+            if (token) {
+                config.headers = config.headers || {};
+                config.headers.Authorization = 'Bearer' + token;
+            }
+            return config;
+        }
+    });
+
 })();
