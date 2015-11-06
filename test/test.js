e309328393f075a72fdc37e680933131733c5796
Shaun McGlinchey <shaunmcglinchey@users.noreply.github.com>
2015-11-06 00:36:49 -0300

    added mocha Authentication API tests

--- test/test.js
+++ test/test.js
@@ -5,18 +5,42 @@ var should = require('chai').should(),
 
 var config = require('../config');
 var api = supertest(config.api.url);
-var thingsUrl = '/api/things';
+var loginUrl = config.api.login_path;
 
 describe('Test API', function() {
 
-    it('should check we can get things from our API', function (done) {
-        api.get(thingsUrl)
-            .expect(200)
-            .end(function (err, res) {
-                console.log(res.body);
-                expect(res.body).to.be.an('array');
-                done();
-            });
+    it('should return a 400 Bad Request response for a call to the login endpoint without any credentials', function (done) {
+        api.post(loginUrl)
+            .send()
+            .expect(400, done);
+    });
+
+    it('should return a 400 Unauthorized response for a call to the login endpoint with an invalid password ', function (done) {
+        var user = {};
+        api.post(loginUrl)
+            .send(user)
+            .expect(400, done);
+    });
+
+    it('should return a 401 Unauthorized response for a call to the login endpoint with an invalid username', function (done) {
+        var user = { username : 'usersd' };
+        api.post(loginUrl)
+            .send(user)
+            .expect(401, done);
+    });
+
+    it('should return a 400 Bad Request response for a call to the login endpoint without a password ', function (done) {
+        var user = { username : 'user' };
+        api.post(loginUrl)
+            .send(user)
+            .expect(400, done);
+    });
+
+    it('should return a 401 Unauthorized response for a call to the login endpoint with an invalid password ', function (done) {
+        var user = { username : 'user', password: 'p' };
+        api.post(loginUrl)
+            .send(user)
+            .expect(401, done);
     });
 
 });
